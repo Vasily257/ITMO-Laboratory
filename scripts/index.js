9290dbed82edae2afb2a9189370a278c0d5c7264
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-04-06 12:26:40 +0400

    feat: add a function to create an article

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,27 @@
 'use strict';
 
 import { articleData } from './articles.js';
+
+function createArticle(data) {
+  const articleElement = document
+    .querySelector('.articles-template')
+    .content.firstElementChild.cloneNode(true);
+
+  const articleImage = articleElement.querySelector('.articles__item-image');
+  const articleTitle = articleElement.querySelector('.articles__item-title');
+  const articleAuthors = articleElement.querySelector('.articles__item-authors');
+  const articleText = articleElement.querySelector('.articles__item-text');
+  const articleShareButton = articleElement.querySelector('.share');
+
+  articleImage.src = data.link;
+  articleImage.type = 'webp';
+  articleTitle.textContent = data.title;
+  articleAuthors.textContent = data.authors;
+  articleText.textContent = data.text;
+
+  function handleShareButton(event) {
+    event.target.classList.toggle('.share_opened');
+  }
+
+  articleShareButton.addEventListener('click', handleShareButton);
+}
